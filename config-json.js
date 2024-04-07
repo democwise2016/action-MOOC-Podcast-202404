d0ee455fb0877f8e52f44164d76c710b8e191d47
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-04-07 22:39:16 +0800

    西元2024年04月07日 (週日) 22時39分16秒 CST

---
+++ config-json.js
@@ -0,0 +1,23 @@
+let CONFIG = {
+  publicURL: `https://democwise2016.github.io/action-MOOC-Podcast-202404/`,
+  publicURLShorten: `https://rb.gy/j56mc5`,  // https://democwise2016.github.io/action-MOOC-Podcast-202404/
+  thumbnailBorderColor: '33CC33',
+  titleAppend: '[MC+]',
+  idAppend: false,
+
+  maxDownloadItemPerFeed: 500000,
+  maxDownloadFeed: 5000000,
+  newArrialMax: 500000,
+  maxDownloadItems: 1000000,
+  maxExcutionMinutes: 15,
+  maxSameTime: 2,
+}
+
+// let test = '1780'
+// CONFIG.publicURL = `https://dangerous-lizard-99.telebit.io/`
+// CONFIG.titleAppend = '[MC+] 20240125-' + test
+// CONFIG.idAppend = '20240125-' + test
+// CONFIG.maxDownloadItemPerFeed = 100
+// CONFIG.debug = true
+
+module.exports = CONFIG
